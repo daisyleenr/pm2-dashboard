e9cb799587d72a04b43e437c4def614aff750ad9
Nara Lee <daisyleenr@gmail.com>
2019-05-04 02:49:12 +0900

    Apply styles of NavBar

--- front/src/components/main/MainMenu.js
+++ front/src/components/main/MainMenu.js
@@ -4,22 +4,51 @@ import styled from "styled-components";
 
 const NavBar = styled.div`
   display: flex;
+  align-items: center;
   background-color: #24292e;
-  color: hsla(0, 0%, 100%, 0.7);
   font-size: 14px;
   line-height: 1.5;
   padding: 16px;
+
+  & a {
+    text-decoration: none;
+    color: #fff;
+    font-size: 1rem;
+    font-weight: 900;
+    white-space: nowrap;
+  }
+
+  & a:hover {
+    color: hsla(0, 0%, 100%, 0.7);
+  }
+
+  & ul {
+    display: flex;
+    margin-left: 30px;
+  }
+
+  & li {
+    margin-right: 30px;
+  }
+`;
+
+const Logo = styled.span`
+  margin: 0 30px;
+  font-size: 1.2rem;
+  font-weight: 900;
+  color: #f39c12;
 `;
 
 const MainMenu = () => {
   return (
     <NavBar>
+      <Logo>FICCY</Logo>
       <ul>
         <li>
-          <Link to="/monitoring/process">프로세스</Link>
+          <Link to="/monitoring/process">Process</Link>
         </li>
         <li>
-          <Link to="/sample">샘플</Link>
+          <Link to="/sample">Sample</Link>
         </li>
       </ul>
     </NavBar>
